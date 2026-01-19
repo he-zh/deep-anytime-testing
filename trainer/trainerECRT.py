038c732ea0174573e59ac05c9e26e2396b50a710
he-zh <zhhe@cs.ubc.ca>
2026-01-18 23:35:45 -0800

    debug ecrt

--- trainer/trainerECRT.py
+++ trainer/trainerECRT.py
@@ -16,9 +16,11 @@ class TrainerECRT(Trainer):
         super().__init__(cfg, net, tau1, tau2, datagen, device, data_seed)
         self.loss = torch.nn.MSELoss(reduction='mean')
         self.integral_vector = torch.tensor(np.linspace(0, 1, 1001, endpoint=False)[1:]).to(device)
-        self.stb = [torch.ones(1000, device=device) for _ in range(3)]
+        self.batches = [2, 5, 10]
+        self.stb = [torch.ones(1000, device=device) for _ in range(len(self.batches))]
 
-    def ecrt(self, y, x, x_tilde, batches, mode):
+
+    def ecrt(self, y, x, x_tilde, mode):
         """
         Implement the ECRT method.
         Code adapted from https://github.com/shaersh/ecrt/
@@ -27,7 +29,6 @@ class TrainerECRT(Trainer):
         - y (torch.Tensor): Ground truth labels.
         - x (torch.Tensor): Input samples.
         - x_tilde (torch.Tensor): Perturbed samples.
-        - batches (list): List of batch sizes.
         - mode (str): Either "train" or "test".
 
         Returns:
@@ -36,28 +37,29 @@ class TrainerECRT(Trainer):
         self.net = self.net.eval()
         total_samples = y.shape[0]
         test_stat = torch.nn.MSELoss(reduction='mean')
-        st, stb = [], []
+        st = []
+        stb = [torch.ones(1000, device=self.device) for _ in range(len(self.batches))]
         i = 0
 
-        for b in batches:
+        for b in self.batches:
             # split data into batches of size b
             num_chunks = int(total_samples / b)
             index_sets_seq = np.array_split(range(total_samples), num_chunks)
-            stb.append(torch.ones(1000))
+            stb.append(torch.ones(1000, device=self.device))
             for ind in index_sets_seq:
                 y_tb, x_tb, x_tilde_tb = y[ind], x[ind], x_tilde[ind]
-                pred_tilde_tb = self.net(x_tilde_tb)
+                pred_tilde_tb = self.net(x_tilde_tb).detach()
                 pred_tb = self.net(x_tb).detach()
 
-                q = test_stat(pred_tb.squeeze(), y_tb)
-                q_tilde = test_stat(pred_tilde_tb.squeeze(), y_tb)
+                q = test_stat(pred_tb, y_tb)
+                q_tilde = test_stat(pred_tilde_tb, y_tb)
                 wealth = torch.nn.Tanh()(q_tilde - q)
 
                 if mode == "test":
-                    self.stb[i] = self.stb[i].to(self.device) * (1 + self.integral_vector * wealth)
+                    self.stb[i] = self.stb[i] * (1 + self.integral_vector * wealth)
                     stb[i] = self.stb[i].clone()
                 else:
-                    stb[i] = stb[i].to(self.device) * (1 + self.integral_vector * wealth)
+                    stb[i] = stb[i] * (1 + self.integral_vector * wealth)
 
             st.append(stb[i].mean())
             i += 1
@@ -88,22 +90,22 @@ class TrainerECRT(Trainer):
             self.net = self.net.train() if mode == "train" else self.net.eval()
 
             out = self.net(features)
-            loss = self.loss(out.squeeze(), target)
+            loss = self.loss(out, target)
             aggregated_loss += loss
 
             y_tilde = self.net(tau_z[:, :-target_size]).detach()
-            loss_tilde += self.loss(y_tilde.squeeze(), target)
+            loss_tilde += self.loss(y_tilde, target).item()
 
             if mode == "train":
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-            e_val = self.ecrt(target, features, tau_z[:, :-target_size], [2, 5, 10], mode)
+            e_val = self.ecrt(target, features, tau_z[:, :-target_size], mode)
 
         self.log({
             f"{mode}_loss": aggregated_loss.item() / (i + 1),
             f"{mode}_e-val": e_val.item(),
-            f"{mode}_loss_tilde": loss_tilde.item() / (i + 1)
+            f"{mode}_loss_tilde": loss_tilde / (i + 1)
         })
         return aggregated_loss / num_samples, e_val
